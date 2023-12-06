05d380fcee2522fbb9d4c01e5b85c44bb965c602
GiangLLT <lepham1622@gmail.com>
2023-12-06 16:34:46 +0700

    API github v1.5

--- my_project/Admin/views.py
+++ my_project/Admin/views.py
@@ -323,7 +323,7 @@ def push_to_main_and_merge(request):
 def github_file_list(request):
     github_username = 'GiangLLT'
     repo_name = 'Django_Service_Desk-main'
-    token = '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'
+    token = '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'
     api_url_dev = f'https://api.github.com/repos/{github_username}/{repo_name}/commits?sha=Dev'
     api_url_main = f'https://api.github.com/repos/{github_username}/{repo_name}/commits?sha=main'
 
